80072ea3ae19f56416ff40ff482a165cf518acbc
ImperialSympathizer <imperialsympathizer@gmail.com>
2024-06-26 13:21:35 -0400

    disable login on beta branch and enable session import

--- src/ui/menu-ui-handler.ts
+++ src/ui/menu-ui-handler.ts
@@ -146,7 +146,7 @@ export default class MenuUiHandler extends MessageUiHandler {
       });
     };
 
-    if (Utils.isLocal) {
+    if (Utils.isLocal || bypassLogin) {
       manageDataOptions.push({
         label: i18next.t("menuUiHandler:importSession"),
         handler: () => {
